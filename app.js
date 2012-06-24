2490679cd32c54bf8a87f8667fcfe21cbe981451
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-06-24 21:11:10 +0300

    Add deployment hook

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var http = require('http');
 
 http.createServer(function(req, res) {
+    if (req.url === '/boogoofoo' && req.method === 'POST') {
+        console.log('Deployment!');
+        setTimeout(function() {
+            process.exit(1);
+        }, 1000);
+        return res.end('OK');
+    }
+
     console.log('request!');
     res.end('hello, node!\n');
 }).listen(process.env.port || 5000);
